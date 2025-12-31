80346abba5bfc042d0b232518aefab7ee6c674bf
Khushi Kumari <165982910+khushikumari90@users.noreply.github.com>
2025-12-31 21:26:08 +0530

    Update HotelBooking.java

--- oops-practice/gcr-codebase/constructors,instance variables,class variables & access modifiers/HotelBooking.java
+++ oops-practice/gcr-codebase/constructors,instance variables,class variables & access modifiers/HotelBooking.java
@@ -5,21 +5,21 @@ public class HotelBooking {
     String roomType;
     int nights;
 
-    // 1️⃣ Default constructor
+    // 1️ Default constructor
     HotelBooking() {
         guestName = "Guest";
         roomType = "Standard";
         nights = 1;
     }
 
-    // 2️⃣ Parameterized constructor
+    // 2️ Parameterized constructor
     HotelBooking(String guestName, String roomType, int nights) {
         this.guestName = guestName;
         this.roomType = roomType;
         this.nights = nights;
     }
 
-    // 3️⃣ Copy constructor
+    // 3️ Copy constructor
     HotelBooking(HotelBooking other) {
         this.guestName = other.guestName;
         this.roomType = other.roomType;
